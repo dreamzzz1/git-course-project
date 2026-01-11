dreamzzz1/git-course-project

commit deda1385adbcb8b4eb0acd56fcc60842a06e166a

refactor: Add entry timestamps

src/main/java/ru/rut/miit/git/Journal.java
@@ -16,7 +16,7 @@ import java.util.stream.Stream;
 
 public class Journal {
     private static final String JOURNAL_FILE = "journal.txt";
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimaFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) {
         if (args.length < 1) {

commit 2983519d5dd7a00b3eb8b6a507bd61a843d54267

revert: refactor: add entry timestamps
Reverts commit deda1385adbcb8b4eb0acd56fcc60842a06e166a which broke add command.
Closes CHRON-404
Revert "refactor: Add entry timestamps"

This reverts commit deda1385adbcb8b4eb0acd56fcc60842a06e166a.

src/main/java/ru/rut/miit/git/Journal.java
@@ -16,7 +16,7 @@ import java.util.stream.Stream;
 
 public class Journal {
     private static final String JOURNAL_FILE = "journal.txt";
-    private static final DateTimaFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) {
         if (args.length < 1) {